7cbd7360e25a54cb4e6bb1da9a720b4fdecdd482
ITS ME ALVI <alviislamofficial2025@gmail.com>
2025-09-18 18:30:47 +0300

    Create alvi.js

---
+++ event/alvi.js
@@ -0,0 +1,24 @@
+// events/alvi.js
+module.exports = async ({ api, event }) => {
+  const logger = require('../main/catalogs/ALVI.js')
+  
+  const configCustom = {
+    autosetbio: { status: false, bio: `prefix : ${global.config.PREFIX}` },
+    greetings: { status: false, morning: `goodmorning everyone, have a nice day.`, afternoon: `goodafternoon everyone, don't forget to eat your lunch.`, evening: `goodevening everyone, don't forget to eat.`, sleep: `goodnight everyone, time to sleep.` },
+    autoDeleteCache: { status: true, time: 10 },
+    autoRestart: { status: false, time: 40 },
+    accpetPending: { status: false, time: 10 }
+  }
+
+  function autosetbio(config) { if (config.status) { try { api.changeBio(config.bio, (err) => { if (err) return logger(`error : ${err}`, 'setbio'); return logger(`changed the bot bio into : ${config.bio}`, 'setbio'); }) } catch (error) { logger(`error : ${error}`, 'setbio') } } }
+  function greetings(config) { if (config.status) { try { const nam = [ { timer: '5:00:00 AM', message: [`${config.morning}`] }, { timer: '11:00:00 AM', message: [`${config.afternoon}`] }, { timer: '6:00:00 PM', message: [`${config.evening}`] }, { timer: '10:00:00 PM', message: [`${config.sleep}`] } ]; setInterval(() => { const r = a => a[Math.floor(Math.random()*a.length)]; if (á = nam.find(i => i.timer == new Date(Date.now()+25200000).toLocaleString().split(/,/).pop().trim())) global.data.allThreadID.forEach(i => api.sendMessage(r(á.message), i)); }, 1000); } catch (error) { logger(`error : ${error}`, 'greetings') } } }
+  function autoDeleteCache(config) { if(config.status) { setInterval(async () => { const { exec } = require('child_process'); exec('rm -rf ../../scripts/commands/cache && mkdir -p ../../scripts/commands/cache && rm -rf ../../scripts/events/cache && mkdir -p ../../scripts/events/cache', (error, stdout, stderr) => { if (error) return logger(`error : ${error}`, "cache"); if (stderr) return logger(`stderr : ${stderr}`, "cache"); return logger(`successfully deleted caches`, "cache"); }) }, config.time * 60 * 1000) } }
+  function autoRestart(config) { if(config.status) { setInterval(async () => { logger(`auto restart is processing, please wait.`, "ryuko"); process.exit(1) }, config.time * 60 * 1000) } }
+  function accpetPending(config) { if(config.status) { setInterval(async () => { const list = [ ...(await api.getThreadList(1, null, ['PENDING'])), ...(await api.getThreadList(1, null, ['OTHER'])) ]; if (list[0]) api.sendMessage('this thread is automatically approved by our system.', list[0].threadID); }, config.time * 60 * 1000) } }
+
+  autosetbio(configCustom.autosetbio)
+  greetings(configCustom.greetings)
+  autoDeleteCache(configCustom.autoDeleteCache)
+  autoRestart(configCustom.autoRestart)
+  accpetPending(configCustom.accpetPending)
+};
